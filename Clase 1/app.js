a34317c0c358d5af058976868796a3882e0400e4
Pablo Llanes <pablo.kaokan@gmail.com>
2024-06-10 22:35:02 -0300

    Clase-1

---
+++ Clase 1/app.js
@@ -0,0 +1,15 @@
+/* este es un comentario */
+// VARIABLES Y TIPOS DE DATOS
+let nombre = "pepe";
+let apellido = "perez"; // ----> string
+let numero = "12";
+// alert(nombre);
+// alert(apellido);
+console.log(nombre);
+console.log(apellido);
+
+const documento = "42748974"; // ----> number
+
+numero = Number(numero);
+
+console.log(numero);
